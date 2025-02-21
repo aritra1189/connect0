3a272dcf68ac9f6daec2be9a5bc79f2ab5d7cd7c
aritra sharma <98529719+aritra1189@users.noreply.github.com>
2025-02-21 11:11:28 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://connect0-1.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://connect0-1.onrender.com/api" : "https://connect0-1.onrender.com/api",
   withCredentials: true,
 });
